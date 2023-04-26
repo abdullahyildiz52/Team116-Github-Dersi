1b230daedc6c4bca14a23eb8166d824030c73173
Abdullah Yıldız <ce_abdullahyildiz@hotmail.com>
2023-04-26 12:55:16 +0300

    ilk commit

---
+++ src/day01/C02.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class C02 {
+    //git init  bir workingDirectory
+    // ısadece 1 kere yapar
+    /*
+    bundan sonra yer yedek alma işleminde commit yapmalıyız
+    aşağıdaki 3 komut bizim dosyalarımızı her yedekleme de
+    kullaanılır
+    1 git add.
+
+     */
+}
