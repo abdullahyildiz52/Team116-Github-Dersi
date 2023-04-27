84335ab02dbd3f38f4978d92eeafbde0df3b5adc
Abdullah Yıldız <ce_abdullahyildiz@hotmail.com>
2023-04-27 13:20:57 +0300

    12. commit

--- src/day01/C06_branchKullanımı.java
+++ src/day01/C06_branchKullanımı.java
@@ -11,5 +11,18 @@ public class C06_branchKullanımı {
 
     git checkout branchAdı: istediğimiz branc e gecer
 
+    oluşturdupğumuz yenib branch de istediğimiz yeni değişiklikler yapt sonra
+    bu değişilkliklerin main branch e işlenmesini istiyorsak
+
+    1- önce main branch e geceriz
+    git checkout main
+
+    2- main branch te dğişiklik
+    git merge deneme
+
+    cakışma olmadığı müddetçe git merge deneme branch indeki tüm değişiklikleri main branch eişler
+
+
+
      */
 }
